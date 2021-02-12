5b4cc020a7d91d02c0e6c36868e2ac8f15da9937
ak-matsu <sandbox.matsu@gmail.com>
2021-02-12 15:35:10 +0900

    検証のためインスタンスを作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
     it 'nicknameが空では登録できない' do
       # nicknameが空では登録できないテストコードを記述します。
+      user = User.new(nickname: '', email: 'test@example', password: '000000', password_confirmation: '000000')
     end
     it 'emailが空では登録できない' do
       # emailが空では登録できないテストコードを記述します。
