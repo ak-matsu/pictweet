8844afa8dd3c66d08d873d1ec678d7826e7e8035
ak-matsu <sandbox.matsu@gmail.com>
2021-02-12 15:59:05 +0900

    expectationを記述、想定通りの挙動を確認する構文

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe User, type: :model do
       # nicknameが空では登録できないテストコードを記述します。
       user = User.new(nickname: '', email: 'test@example', password: '000000', password_confirmation: '000000')
       user.valid?
+      expect(user.errors.full_messages).to include("Nickname can't be blank")
     end
     it 'emailが空では登録できない' do
       # emailが空では登録できないテストコードを記述します。
