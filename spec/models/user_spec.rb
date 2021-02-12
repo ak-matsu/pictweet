d328023ad0d438374144fd1cbbbb0015f93bf3a8
ak-matsu <sandbox.matsu@gmail.com>
2021-02-12 15:27:57 +0900

    2つのexsample(nickname,emailが空では登録できない)記述を追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
-    # ユーザー新規登録についてのテストコードを記述します。
+    it 'nicknameが空では登録できない' do
+      # nicknameが空では登録できないテストコードを記述します。
+    end
+    it 'emailが空では登録できない' do
+      # emailが空では登録できないテストコードを記述します。
+    end
   end
 end
