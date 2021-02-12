a55a841659aa379af8fed75bdb485f84a58810c1
ak-matsu <sandbox.matsu@gmail.com>
2021-02-12 15:19:26 +0900

    describeメソッドを用いてテスト内容を記述

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー新規登録' do
+    # ユーザー新規登録についてのテストコードを記述します。
+  end
 end
