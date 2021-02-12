a91c70df4d768a1ba2fe06ea347c5647514bb803
ak-matsu <sandbox.matsu@gmail.com>
2021-02-12 21:02:58 +0900

    beforeで類似した記述方法をまとめた

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
   describe 'ユーザー新規登録' do
     it 'nicknameが空では登録できない' do
-      user = FactoryBot.build(:user) #Userのインスタンス生成
-      user.nickname = '' #nicknameの値を空にする
-      user.valid?
-      expect(user.errors.full_messages).to include("Nickname can't be blank")
+      @user.nickname = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Nickname can't be blank"
     end
     it 'emailが空では登録できない' do
-      user = FactoryBot.build(:user) # Userのインスタンス作成
-      user.email = '' #emailの値を空にする
-      user = User.new(nickname: 'test', email: '', password: '000000', password_confirmation: '000000')
-      user.valid?
-      expect(user.errors.full_messages).to include("Email can't be blank")
+      @user.email = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Email can't be blank"
     end
   end
 end
