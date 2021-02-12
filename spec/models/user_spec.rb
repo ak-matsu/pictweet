a72295e19c09ac3d90bca0e766eb30cb10919c23
ak-matsu <sandbox.matsu@gmail.com>
2021-02-12 20:44:17 +0900

    FactoryBotを使ってインスタンスを生成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,13 +3,14 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
     it 'nicknameが空では登録できない' do
-      # nicknameが空では登録できないテストコードを記述します。
-      user = User.new(nickname: '', email: 'test@example', password: '000000', password_confirmation: '000000')
+      user = FactoryBot.build(:user) #Userのインスタンス生成
+      user.nickname = '' #nicknameの値を空にする
       user.valid?
       expect(user.errors.full_messages).to include("Nickname can't be blank")
     end
     it 'emailが空では登録できない' do
-      # emailが空では登録できないテストコードを記述します。
+      user = FactoryBot.build(:user) # Userのインスタンス作成
+      user.email = '' #emailの値を空にする
       user = User.new(nickname: 'test', email: '', password: '000000', password_confirmation: '000000')
       user.valid?
       expect(user.errors.full_messages).to include("Email can't be blank")
