44039a0d700425f503137d85dfc03990dbe5ac0c
ak-matsu <sandbox.matsu@gmail.com>
2021-02-12 15:00:14 +0900

    nicknameにバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :tweets
   has_many :comments #commentsテーブルとのアソシエーション
+
+  validates :nickname, presence: true
 end
