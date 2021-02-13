fc1c0b3f0e2289b06d15731771e9b5d9442c3e04
ak-matsu <sandbox.matsu@gmail.com>
2021-02-13 20:35:58 +0900

    ユーザーが紐付いていないと投稿できないテストコード

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -24,6 +24,10 @@ RSpec.describe Tweet, type: :model do
         expect(@tweet.errors.full_messages).to include("Text can't be blank")
       end
       it 'ユーザーが紐付いていなければ投稿できない' do
+        @tweet.user = nil #生成した@tweetに紐づくユーザーを、nilとして無いものとしている。
+        @tweet.valid?
+        # binding.pry
+        expect(@tweet.errors.full_messages).to include("User must exist")
       end
     end
   end
