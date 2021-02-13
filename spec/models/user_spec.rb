46c02117f213b65e7a96b7887253dab8abadf648
ak-matsu <sandbox.matsu@gmail.com>
2021-02-13 15:22:15 +0900

    emailとpasswordが空だと登録できない記述を追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,8 +24,14 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("Nickname can't be blank")
     end
     it 'emailが空では登録できない' do
+      @user.email = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email can't be blank")
     end
     it 'passwordが空では登録できない' do
+      @user.password = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password can't be blank")
     end
     it 'passwordが存在してもpassword_confirmationが空では登録できない' do
     end
