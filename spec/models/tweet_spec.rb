a66d49d55cafcb632255d087b59e37e69a1f0f89
ak-matsu <sandbox.matsu@gmail.com>
2021-02-13 20:11:15 +0900

    beforeとexsampleを記述

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Tweet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @tweet = FactoryBot.build(:tweet)
+  end
+
+  describe 'ツイートの保存' do
+    context 'ツイートが投稿できる場合' do
+      it '画像とテキストがあれば投稿できる' do
+      end
+      it 'テキストがあれば投稿できる' do
+      end
+    end
+
+    context 'ツイートが投稿できない場合' do
+      it 'テキストが空では投稿できない' do
+      end
+      it 'ユーザーが紐付いていなければ投稿できない' do
+      end
+    end
+  end
 end
