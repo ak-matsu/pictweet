af3697bc1873c179b07c4e61367b82bf1e6b2b50
ak-matsu <sandbox.matsu@gmail.com>
2021-02-13 18:08:21 +0900

    passwordが5文字以下では登録できない。

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -51,6 +51,10 @@ RSpec.describe User, type: :model do
       expect(another_user.errors.full_messages).to include('Email has already been taken')
     end
     it 'passwordが5文字以下では登録できない' do
+      @user.password = '00000'
+      @user.password_confirmation = '00000'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Password is too short (minimum is 5 characters)')
     end
   end
 end
