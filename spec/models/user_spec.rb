66ac0614f625b3feec62cc103162adafb60830e2
ak-matsu <sandbox.matsu@gmail.com>
2021-02-13 14:10:00 +0900

    列挙したexampleを記述

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,15 +6,25 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
+    it 'nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる' do
+    end
+    it 'nicknameが6文字以下であれば登録できる' do
+    end
+    it 'passwordとpassword_confirmationが6文字以上であれば登録できる' do
+    end
     it 'nicknameが空では登録できない' do
-      @user.nickname = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include "Nickname can't be blank"
     end
     it 'emailが空では登録できない' do
-      @user.email = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include "Email can't be blank"
+    end
+    it 'passwordが空では登録できない' do
+    end
+    it 'passwordが存在してもpassword_confirmationが空では登録できない' do
+    end
+    it 'nicknameが7文字以上では登録できない' do
+    end
+    it '重複したemailが存在する場合登録できない' do
+    end
+    it 'passwordが5文字以下では登録できない' do
     end
   end
 end
