757cfe68f0cd1b279ecfc7f802a363e15b64bfbd
ak-matsu <sandbox.matsu@gmail.com>
2021-02-14 14:43:21 +0900

    同じように投稿検索フォームも記述
    response.bodyで表示されたhtmlの結果の中に、「投稿を検索する」という文字列があることを確認するitです。

--- spec/requests/tweets_spec.rb
+++ spec/requests/tweets_spec.rb
@@ -22,6 +22,8 @@ RSpec.describe "Tweets", type: :request do
       expect(response.body).to include(@tweet.image)
     end
     it 'indexアクションにリクエストするとレスポンスに投稿検索フォームが存在する' do
+      get root_path
+      expect(response.body).to include('投稿を検索する')
     end
   end
 end
