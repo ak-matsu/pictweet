4d9e8d7923d95e392d95cb7593fe754bb96815e8
ak-matsu <sandbox.matsu@gmail.com>
2021-02-14 19:42:58 +0900

    残りのexsampleを記述

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -52,15 +52,29 @@ RSpec.describe 'ユーザー新規登録', type: :system do
 
   context 'ユーザ新規登録ができないとき' do
     it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
+      
+      #トップページに移動する
+      visit root_path
+
+      #トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('新規登録')
     
-    #トップページに移動する
-    #トップページにサインアップページへ遷移するボタンがある
-    #新規登録ページへ移動する
-    #ユーザー情報を入力する
-    #サインアップボタンを押してもユーザーモデルのカウントは上がらない
-    #新規登録ページへ戻される
+      #新規登録ページへ移動する
+      visit new_user_registration_path
+
+      #ユーザー情報を入力する
+      fill_in 'Nickname', with: ''
+      fill_in 'Email',with: ''
+      fill_in 'Password',with: ''
+      fill_in 'Password confirmation',with:''
+
+      #サインアップボタンを押してもユーザーモデルのカウントは上がらない
+      expect{
+        find('input[name="commit"]').click
+      }.to change { User.count }.by(0)
+
+      #新規登録ページへ戻される
+      expect(current_path).to eq('/users')
     end
   end
-
-
 end
