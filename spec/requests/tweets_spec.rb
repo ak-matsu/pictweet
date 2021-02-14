599fa3a7bf1e269b7834153178db29d5b19b5ef1
ak-matsu <sandbox.matsu@gmail.com>
2021-02-14 13:41:29 +0900

    beforeとexsampleを記述
    コントローラーテストを行うため、テンプレートを作成

--- spec/requests/tweets_spec.rb
+++ spec/requests/tweets_spec.rb
@@ -1,10 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "Tweets", type: :request do
-  describe "GET /tweets" do
-    it "works! (now write some real specs)" do
-      get tweets_index_path
-      expect(response).to have_http_status(200)
+  before do #beforeの後に共通している部分を前もって定義しておく
+    @tweet = FactoryBot.create(:tweet) #createメソッドをつけるとロールバックされ保存された値がすべて消去される。テストのたびにDB保存をさせないため。
+  end
+
+  #exsampleを整理した処理
+  describe "GET #index" do
+    it 'indexアクションにリクエストすると正常にレスポンスが返ってくる' do
+    end
+    it 'indexアクションにリクエストするとレスポンスに投稿済みのツイートのテキストが存在する' do
+    end
+    it 'indexアクションにリクエストするとレスポンスに投稿済みのツイートの画像URLが存在する' do
+    end
+    it 'indexアクションにリクエストするとレスポンスに投稿検索フォームが存在する' do
     end
   end
 end
