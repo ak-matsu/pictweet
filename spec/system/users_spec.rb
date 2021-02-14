62373fc57359c9242cc242aa24533e475f2cf078
ak-matsu <sandbox.matsu@gmail.com>
2021-02-14 16:56:56 +0900

    修正
    写経、記述ミスにより修正

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :system do
+RSpec.describe 'ユーザー新規登録', type: :system do
   before do
     @user = FactoryBot.build(:user)
   end
 
-  describe 'ユーザ新規登録ができるとき' do
+  context 'ユーザ新規登録ができるとき' do
     it '正しい情報を入力すればユーザー新規登録ができてトップページへ移動する'do
-    end
+    
     #トップページに移動する
     visit root_path #visit は root_pathへ遷移することを表す
 
@@ -23,17 +23,19 @@ RSpec.describe "Users", type: :system do
     #トップページへ遷移する
     #カーソルを合わせるとログアウトボタンが表示される
     #サインアップボタンへ遷移するボタンや、ログインページへ遷移するボタンが表示されていない
+    end
   end
 
-  describe 'ユーザ新規登録ができないとき' do
+  context 'ユーザ新規登録ができないとき' do
     it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
-    end
+    
     #トップページに移動する
     #トップページにサインアップページへ遷移するボタンがある
     #新規登録ページへ移動する
     #ユーザー情報を入力する
     #サインアップボタンを押してもユーザーモデルのカウントは上がらない
     #新規登録ページへ戻される
+    end
   end
 
 
