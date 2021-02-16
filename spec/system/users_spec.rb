707e8a8bf99dc60e2088130d4588b43482bdee8c
ak-matsu <sandbox.matsu@gmail.com>
2021-02-16 20:16:27 +0900

    ログインができる時を記述
    RSpecをRspecなど、コードのうち間違えも修正

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -80,20 +80,40 @@ RSpec.describe 'ユーザー新規登録', type: :system do
 end
 
 #ユーザーログインの結合テストコード
-Rspec.describe 'ログイン', type: :system do
+RSpec.describe 'ログイン', type: :system do
+  #すでに登録されているユーザーに対しての挙動確認はFactoryBot.create(:user)としています。
   before do
     @user = FactoryBot.create(:user)
   end
   context 'ログインができるとき' do
     it '保存されているユーザーの情報と合致すればログインができる' do
       #トップページに移動できる
-      #トップページにログインページへ遷移するボタンがああることを確認する
+      visit root_path
+
+      #トップページにログインページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('ログイン')
+
       #ログインページへ遷移する
+      visit new_user_session_path
+
       #正しいユーザー情報を入力する
+      fill_in 'Email', with: @user.email
+      fill_in 'Password',with: @user.password
+
       #ログインボタンを押す
+      find('input[name="commit"]').click
+
       #トップページへ遷移することを確認する
+      expect(current_path).to eq(root_path)
+
       #カーソルを合わせるとログアウトボタンが表示されることを確認する
+      expect(
+        find('.user_nav').find('span').hover
+      ).to have_content('ログアウト')
+
       #サインアップページへ遷移するボタンやログインページへ遷移するボタンが表示されていないことを確認する
+      expect(page).to have_no_content('新規登録')
+      expect(page).to have_no_content('ログイン')
     end
   end
   context 'ログインができないとき' do
