888430b6befb965903d647f6326f81febaf9a8e9
ak-matsu <sandbox.matsu@gmail.com>
2021-02-16 20:25:16 +0900

    ログインができないときのテストコードを記述

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -94,7 +94,7 @@ RSpec.describe 'ログイン', type: :system do
       expect(page).to have_content('ログイン')
 
       #ログインページへ遷移する
-      visit new_user_session_path
+      visit new_user_session_path #限定したページを参照させるPath
 
       #正しいユーザー情報を入力する
       fill_in 'Email', with: @user.email
@@ -119,10 +119,23 @@ RSpec.describe 'ログイン', type: :system do
   context 'ログインができないとき' do
     it '保存されているユーザーの情報と合致しないとログインができない' do
       #トップページに移動する
+      visit root_path
+
       #トップページにログインページへ遷移するボタンが有ることを確認する
+      expect(page).to have_content('ログイン')
+
       #ログインページへ遷移する
+      visit new_user_session_path
+
       #ユーザー情報を入力する
+      fill_in 'Email',with: ''
+      fill_in 'Password',with: ''
+      
+      #ログインボタンを押す
+      find('input[name="commit"]').click
+
       #ログインページへ戻されることを確認する
+      expect(current_path).to eq(new_user_session_path)
     end
   end
 end
