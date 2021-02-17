269c38f66f8b8d709482e44cc4f739937bff6a1a
ak-matsu <sandbox.matsu@gmail.com>
2021-02-17 19:57:22 +0900

    テストコードの雛形作成

--- spec/system/tweets_spec.rb
+++ spec/system/tweets_spec.rb
@@ -1,9 +1,30 @@
 require 'rails_helper'
 
-RSpec.describe "Tweets", type: :system do
+RSpec.describe "ツイート投稿", type: :system do
   before do
-    driven_by(:rack_test)
+    @user = FactoryBot.create(:user)
+    @tweet_text = Faker::Lorem.sentence
+    @tweet_image = Faker::Lorem.sentence
   end
-
-  pending "add some scenarios (or delete) #{__FILE__}"
+  context 'ツイート投稿ができるとき' do
+    it 'ログインしたユーザーは新規登録ができる' do
+      # ログインする
+      binding.pry
+      # 新規投稿ページへのリンクがあることを確認する
+      # 投稿ページに移動する
+      # フォームに情報を入力する
+      # 送信するとTweetモデルのカウントが1上がることを確認する
+      # 投稿完了ページに遷移することを確認する
+      # 「投稿が完了しました」の文字があることを確認する
+      # トップページに遷移する
+      # トップページには先ほど投稿した内容のツイートが存在することを確認する（画像）
+      # トップページには先ほど投稿した内容のツイートが存在することを確認する（テキスト）
+    end
+  end
+  context 'ツイート投稿ができないとき' do
+    it 'ログインしてないと新規投稿ページ遷移できない' do
+      # トップページに遷移する
+      # 新規投稿ページへのリンクがない
+    end
+  end
 end
