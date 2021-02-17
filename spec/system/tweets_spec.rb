5f7653e0d19fa212af376fc31d572408acd0872c
ak-matsu <sandbox.matsu@gmail.com>
2021-02-17 22:41:21 +0900

    ツイート編集できるときのテストコードを記述

--- spec/system/tweets_spec.rb
+++ spec/system/tweets_spec.rb
@@ -65,18 +65,54 @@ RSpec.describe 'ツイート編集', type: :system do
   context 'ツイート編集できるとき' do
     it 'ログインしたユーザーは自分が投稿したツイートの編集ができる' do      
       # ツイート1を投稿したユーザーでログインする
-      binding.pry
-      visit 
+
+      visit new_user_session_path
+      fill_in "Email",with:@tweet1.user.email
+      fill_in "Password",with:@tweet1.user.password
+      find('input[name="commit"]').click
+
+      expect(current_path).to eq(root_path) 
+      #クリックされログイン後の画面は、
+      #root_pathに戻ってくることを確認する処理。
+
       # ツイート1に「編集」ボタンがあることを確認する
+      expect(
+        all('.more')[1].hover
+      ).to have_link '編集', href: edit_tweet_path(@tweet1)
+
       # 編集ページへ遷移する
+      visit edit_tweet_path(@tweet1)
+
       # すでに投稿済みの内容がフォームに入っていることを確認する
+      expect(
+        find('#tweet_image').value #tweet_imageというid名が付与された要素の値を取得
+      ).to eq(@tweet1.image)
+
+      expect(
+        find('#tweet_text').value #tweet_textというid名が付与された要素の値を取得
+      ).to eq(@tweet1.text)
+      
       # 投稿内容を編集する
+      fill_in 'tweet_image', with: "#{@tweet1.image}+編集した画像URL"
+      fill_in 'tweet_text',with: "#{@tweet1.text}+編集したテキスト"
+
       # 編集してもTweetモデルのカウントは変わらないことを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change {Tweet.count}.by(0)
+
       # 編集完了画面に遷移したことを確認する
+      expect(current_path).to eq(tweet_path(@tweet1))
+
       # 「更新が完了しました」の文字があることを確認する
+      expect(page).to have_content('更新が完了しました')
       # トップページに遷移する
+      visit root_path
       # トップページには先ほど変更した内容のツイートが存在することを確認する（画像）
+      expect(page).to have_selector ".content_post[style='background-image: url(#{@tweet1.image}+編集した画像URL);']"
+
       # トップページには先ほど変更した内容のツイートが存在することを確認する（テキスト）    
+      expect(page).to have_content("#{@tweet1.text}+編集したテキスト")
     end
   end
 
