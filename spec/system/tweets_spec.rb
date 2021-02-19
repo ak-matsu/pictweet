b06565638a025a884d77b07f1e5c2ee0a67e8b69
ak-matsu <sandbox.matsu@gmail.com>
2021-02-19 20:34:47 +0900

    bining.pry削除

--- spec/system/tweets_spec.rb
+++ spec/system/tweets_spec.rb
@@ -9,7 +9,6 @@ RSpec.describe 'ツイート投稿', type: :system do
   context 'ツイート投稿ができるとき'do
     it 'ログインしたユーザーは新規投稿できる' do
       # ログインする
-      binding.pry
       visit user_session_path
       fill_in "Email",with:@user.email
       fill_in "Password",with:@user.password
