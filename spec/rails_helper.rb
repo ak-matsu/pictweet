b8266b6b375e6e69adb1d14ecb32d356fb79ca5b
ak-matsu <sandbox.matsu@gmail.com>
2021-02-20 20:15:30 +0900

    タイプミス修正

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,7 +31,7 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
-  confit.include SignInSupport
+  config.include SignInSupport
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
