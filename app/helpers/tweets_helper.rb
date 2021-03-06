d87d50e499439ecb42737d1f7d22788a56ed1c06
ak-matsu <sandbox.matsu@gmail.com>
2021-03-06 20:37:24 +0900

    ヘルパーメソッドの呼び出し
    This reverts commit 7f0391ba9dd0f3ed9a5003629e3c41dec0d36d1a.

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -1,15 +1,2 @@
 module TweetsHelper
-  # tweet_listsメソッドにtweetsという引数をもたせている
-  def tweet_lists(tweets)
-    # 空の箱としてhtmlを生成
-    html  = ''
-    tweets.each do |tweet|
-      # renderメソッドは部分テンプレートを呼び出す。
-      # partial:オプションで該当する部分テンプレートを表示。
-      # locals:オプションで部分テンプレートの変数を使える。
-      # tweetの部分テンプレートを呼び出し、表示、tweetという変数を使う
-      html +=  render(partial: "tweet", locals: { tweet: tweet })
-    end
-    return raw(html)
-  end
 end
