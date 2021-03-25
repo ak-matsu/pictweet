99c8dc06e25ee022a143f525bd6ba662f4864ea3
ak-matsu <sandbox.matsu@gmail.com>
2021-03-25 20:54:51 +0900

    SQLインジェクションの対策

--- app/services/search_tweets_service.rb
+++ app/services/search_tweets_service.rb
@@ -3,10 +3,9 @@ class SearchTweetsService
   # もし検索フォームに何も入力せず検索ボタンを押すと、引数は空になり、elseが処理される
   # この場合、Tweet.allで全てのTweetを取得、表示させる。
     if search != ""
-    # whereで条件をつけてLIKEであいまい検索
-      # Tweet.where('text LIKE(?)', "%#{search}%")
-      Tweet.find_by_sql("select * from tweets where text like '%#{search}%' ")
-
+      # SQLインジェクションの対策
+      search = "%#{search}%"
+      Tweet.find_by_sql(["select * from tweets where text like ? ", search])
     else
       Tweet.all
     end
