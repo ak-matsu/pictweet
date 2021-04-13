7f8b1f8f2430ddbd62d3d22fea65b17abb4f7a2b
ak-matsu <sandbox.matsu@gmail.com>
2021-04-13 23:13:55 +0900

    showコントローラ個人ページの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
-    @nickname = current_user.nickname
-    @tweets = current_user.tweets
+    @nickname = user.nickname
+    @tweets = user.tweets
   end
 end
